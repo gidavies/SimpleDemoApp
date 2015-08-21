083c4cbef950578b3e39327db98ce1522bca3bc8
Colin Beales <colinbeales@hotmail.com>
2015-08-21 13:52:46 +0000

    Adding MVC App. Wizard standard with Unit Tests and Individual Account access control.

---
+++ SimpleDemoApp/SimpleDemoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleDemoApp.Startup))]
+namespace SimpleDemoApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
