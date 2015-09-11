bde0d5fc6b77a4d57546df3f8f537a776db09ecf
Giles Davies <giles.j.davies@hotmail.com>
2015-09-11 16:45:43 +0100

    Add MyScript.ps1
    An example PowerShell script to add to builds

---
+++ SimpleDemoApp/SimpleDemoApp/DeployTasks/MyScript.ps1
@@ -0,0 +1,13 @@
+# An example PowerShell script.
+# 
+# This script could be doing anything that you can script.
+# It might be an existing script that you can reuse.
+# 
+# An example might be to carry out a deployment step in an on-premise environment  
+
+Write-Host "MyScript.ps1"
+Write-Host "************"
+Write-Host "This could be doing whatever you can script in PowerShell"
+Write-Host "Either on-premise or in the cloud"
+Write-Host "And could be reusing a script you already have"
+Write-Host "*********************************************************"
